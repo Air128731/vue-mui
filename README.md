8c040ba07ff8f91c3e7e0317be8f1991799f93ed
Air128731 <henry.scott0x8080@gmail.com>
2023-03-29 11:08:22 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,79 @@
+# vue-awesome-mui
+[![npm package](https://img.shields.io/npm/v/vue-awesome-mui.svg)](https://www.npmjs.com/package/vue-awesome-mui)
+[![NPM downloads](http://img.shields.io/npm/dm/vue-awesome-mui.svg)](https://www.npmjs.com/package/vue-awesome-mui)
+<a href="https://github.com/Wscats/vue-awesome-mui"><img src="https://img.shields.io/badge/Github Page-Wscats-yellow" alt="Github Page" /></a>
+<a href="https://github.com/Wscats/vue-awesome-mui"><img src="https://img.shields.io/badge/Author-Eno Yao-blueviolet" alt="Eno Yao" /></a>
+
+> Design Mui library for Vuejs 2.x
+
+## Links
+* [Demo Page](https://wscats.github.io/vue-awesome-mui/public/#/index)
+* [Mui中文文档](http://dev.dcloud.net.cn/mui/ui/)
+
+## Install
+
+Install vue-awesome-mui
+```bash
+npm install vue-awesome-mui -save
+```
+
+## Get Started
+
+Vue mount
+
+```javascript
+// import or require
+import Vue from 'vue'
+import Mui from 'vue-awesome-mui'
+import 'vue-awesome-mui/mui/dist/css/mui.css'
+// mount with global
+Vue.use(Mui)
+```
+
+If you need icon, you can require `icon.css`
+```javascript
+import "vue-awesome-mui/mui/examples/hello-mui/css/icons-extra.css";
+```
+
+## Example
+
+Give a few examples
+
+|Vue components|Mui document|
+|-|-|
+|[折叠面板](https://github.com/Wscats/vue-awesome-mui/blob/master/app/components/examples/accordion.vue)|[Accordion](http://dev.dcloud.net.cn/mui/ui/#accordion)|
+|[操作表](https://github.com/Wscats/vue-awesome-mui/blob/master/app/components/examples/actionsheet.vue)|[Actionsheet](http://dev.dcloud.net.cn/mui/ui/#actionsheet)|
+|[数字角标](https://github.com/Wscats/vue-awesome-mui/blob/master/app/components/examples/badges.vue)|[Badges](http://dev.dcloud.net.cn/mui/ui/#badges)|
+|[按钮](https://github.com/Wscats/vue-awesome-mui/blob/master/app/components/examples/buttons-with-badges.vue)|[Buttons](http://dev.dcloud.net.cn/mui/ui/#buttons)|
+
+## Browser Support
+
+* IE 10+
+* Andorid 4.4+
+* IOS 7+
+
+## Changelog
+
+Detailed changes for each release are documented in the [release notes](https://github.com/Wscats/vue-awesome-mui).
+
+## Contribution
+
+Please make sure to read the [Contributing Guide](https://github.com/Wscats/vue-awesome-mui) before making a pull request.
+
+## Dependencies
+
+* [vuejs 2.x](https://vuejs.org/)
+* [mui 3.5](https://github.com/dcloudio/mui)
+
+## Licence
+
+vue-awesome-mui is open source and released under the MIT Licence.
+
+Copyright (c) 2017 [Wscats](https://github.com/Wscats)
+
+## About author
+
+* [Github](https://github.com/Wscats)
+* [Segmentfault](https://segmentfault.com/u/enoy)
+* [CSDN](http://blog.csdn.net/qq_27080247)
+* [Juejin](https://juejin.im/user/584c7f44ac502e0069275cd7)
---
+++ app/main.js
@@ -0,0 +1,198 @@
+//引入vue框架
+import Vue from "vue";
+//引入路由
+import VueRouter from 'vue-router';
+//引入vuex状态管理
+import Vuex from 'vuex';
+//axios的ajax封装库
+import axios from "axios";
+
+/*import Mui from "mui";
+import "mui/dist/css/mui.css"*/
+
+import Mui from "vue-awesome-mui";
+import "vue-awesome-mui/mui/dist/css/mui.css";
+Vue.use(Mui);
+
+//console.log(Vue.prototype);
+Vue.use(Vuex);
+//通过 Vue.use()明确地安装路由功能
+Vue.use(VueRouter);
+//挂载axios在Vue构造器下
+Vue.prototype.$ajax = axios;
+
+//引入组件
+import index from "./components/index.vue";
+//A
+import accordion from "./components/examples/accordion.vue";
+import actionsheet from "./components/examples/actionsheet.vue";
+//B
+import badges from "./components/examples/badges.vue";
+import buttons from "./components/examples/buttons.vue";
+import buttonsWithIcons from "./components/examples/buttons-with-icons.vue";
+import buttonsWithBadges from "./components/examples/buttons-with-badges.vue";
+import buttonsWithBlock from "./components/examples/buttons-with-block.vue";
+import buttonsWithLoading from "./components/examples/buttons-with-loading.vue"
+//C
+import card from "./components/examples/card.vue";
+import checkbox from "./components/examples/checkbox.vue";
+//D
+import dtpicker from "./components/examples/dtpicker.vue";
+import dialog from "./components/examples/dialog.vue";
+//S
+import sliderDefault from "./components/examples/slider-default.vue";
+import sliderTableDefault from "./components/examples/slider-table-default.vue";
+import sliderWithTitle from "./components/examples/slider-with-title.vue";
+//G
+import gridDefault from "./components/examples/grid-default.vue";
+import gridPagination from "./components/examples/grid-pagination.vue";
+//I
+import icons from "./components/examples/icons.vue";
+import iconsExtra from "./components/examples/icons-extra.vue";
+import input from "./components/examples/input.vue";
+import mediaList from "./components/examples/media-list.vue";
+//N
+import nav from "./components/examples/nav.vue";
+import navTransparent from "./components/examples/nav_transparent.vue";
+import numbox from "./components/examples/numbox.vue";
+
+var router = new VueRouter({
+	routes: [{
+		path: '/index',
+		component: index,
+	}, {
+		path: '/accordion',
+		component: accordion,
+	}, {
+		path: '/actionsheet',
+		component: actionsheet,
+	}, {
+		path: '/badges',
+		component: badges,
+	}, {
+		path: '/buttons',
+		component: buttons,
+	}, {
+		path: '/buttons-with-icons',
+		component: buttonsWithIcons,
+	}, {
+		path: '/buttons-with-badges',
+		component: buttonsWithBadges,
+	}, {
+		path: '/buttons-with-block',
+		component: buttonsWithBlock,
+	}, {
+		path: '/buttons-with-loading',
+		component: buttonsWithLoading,
+	}, {
+		path: '/card',
+		component: card,
+	}, {
+		path: '/checkbox',
+		component: checkbox,
+	}, {
+		path: '/dtpicker',
+		component: dtpicker,
+	}, {
+		path: '/dialog',
+		component: dialog,
+	}, {
+		path: '/sliderDefault',
+		component: sliderDefault,
+	}, {
+		path: '/sliderTableDefault',
+		component: sliderTableDefault,
+	}, {
+		path: '/sliderWithTitle',
+		component: sliderWithTitle,
+	}, {
+		path: '/gridDefault',
+		component: gridDefault,
+	}, {
+		path: '/gridPagination',
+		component: gridPagination,
+	}, {
+		path: '/icons',
+		component: icons,
+	}, {
+		path: '/iconsExtra',
+		component: iconsExtra,
+	}, {
+		path: '/input',
+		component: input,
+	}, {
+		path: '/mediaList',
+		component: mediaList,
+	}, {
+		path: '/nav',
+		component: nav,
+	}, {
+		path: '/navTransparent',
+		component: navTransparent,
+	}, {
+		path: '/numbox',
+		component: numbox,
+	}, {
+		path: '/',
+		redirect: '/index'
+	}]
+});
+
+//新建一个状态管理
+var store = new Vuex.Store({
+	//定义一个状态
+	//所有组件的状态，也就是数据源
+	state: {
+		count: 1,
+		title: "标题",
+		search: "",
+		news: null,
+		imgUrl: null,
+		isShowGallery: false,
+		direction: "left"
+	},
+	getters: {
+		getCount(state) {
+			//处理数据
+			return state.count + "ed"
+		}
+	},
+	//分发状态
+	mutations: {
+		setCount(state, data) {
+			state.count = data
+
+		},
+		settitle(state, data) {
+			state.title = data
+		},
+		setNews(state) {
+			axios.get('https://cnodejs.org/api/v1//topics')
+				.then((response) => {
+					state.news = response.data.data
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		}
+	},
+	//action就是触发mutations
+	actions: {
+		setChange(context, data) {
+			context.commit('setCount', data)
+			context.commit('settitle', data)
+		},
+		setNews(context, data) {
+			context.commit('setNews')
+		}
+	}
+})
+
+new Vue({
+	el: "#demo",
+	template: `
+		<router-view></router-view>
+	`,
+	router,
+	store,
+})
